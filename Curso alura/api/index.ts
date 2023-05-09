38c87e56c1cbd4b8e9a85386b9276477e9492a8c
Pedro Gabriel <pedrpg5@gmail.com>
2023-05-09 14:51:08 -0300

    Progredindo em graphql

--- Curso alura/api/index.ts
+++ Curso alura/api/index.ts
@@ -1,20 +1,43 @@
 import { ApolloServer } from 'apollo-server';
+import path from 'path';
+
+// Schemas
 const userSchema = require('./User/schema/user.graphql');
+const turmaSchema = require('./Turma/schema/turma.graphql');
+
+// Resolvers
 import { userResolvers } from './User/resolvers/userResolvers';
+import { turmasResolvers } from './Turma/resolvers/turmaResolvers';
+
+// Controllers
 import UsersApi from './User/datasource/user';
+import TurmasAPI from './Turma/datasource/turma';
 
 const typeDefs = [
     userSchema,
+    turmaSchema
 ];
 
-const resolvers = [userResolvers];
+const resolvers = [
+    userResolvers,
+    turmasResolvers
+];
+
+const dbConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+        filename: path.resolve(__dirname, './data/database.db')
+    }
+}
 
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
     dataSources: () => {
         return {
-            usersApi: new UsersApi()
+            usersApi: new UsersApi(),
+            turmasApi: new TurmasAPI(dbConfig)
         }
     }
 });
